bd1ebaac6505a7f1ca5d7cd1c3749a65e6bdcbb0
unknown <stephanie.-@edu.devinci.fr>
2024-01-23 17:51:38 +0100

    Your commit message here

--- lessons/01-model-and-experiment-management/app_config.py
+++ lessons/01-model-and-experiment-management/app_config.py
@@ -1,7 +1,8 @@
 # MODELS
 MODEL_VERSION = "0.0.1"
-PATH_TO_PIPELINE = ...  # TODO: fill in the path to the pipeline
-
+PATH_TO_PREPROCESSOR = f"C:/Git/Stephanie/stephina/lessons/02-model-deployment/web_service/local_models/dv_v/{MODEL_VERSION}.pkl"
+PATH_TO_MODEL = f"/C:/Git/Stephanie/stephina/lessons/02-model-deployment/web_service/local_models/model_v/{MODEL_VERSION}.pkl"
+CATEGORICAL_COLS = ["PUlocationID", "DOlocationID", "passenger_count"]
 
 # MISC
 APP_TITLE = "TripDurationPredictionApp"

abee99d5891455a60f69d2b878677754942a05ba
unknown <stephanie.-@edu.devinci.fr>
2024-01-23 19:42:08 +0100

    Your commit message here

--- lessons/02-model-deployment/web_service/app_config.py
+++ lessons/02-model-deployment/web_service/app_config.py
@@ -1,6 +1,8 @@
 # MODELS
 MODEL_VERSION = "0.0.1"
-PATH_TO_PIPELINE = "C:\Git\Stephanie\stephina\lessons\02-model-deployment\web_service\local_models\model_v\model.pkl"  # TODO: fill in the path to the pipeline
+PATH_TO_PIPELINE = "C:\Git\Stephanie\stephina\lessons\02-model-deployment\web_service\local_models\model_v\model.pkl"  
+# TODO: fill in the path to the pipeline
+PATH_TO_PREPROCESSOR = "C:\Git\Stephanie\stephina\lessons\02-model-deployment\web_service\local_models\dv_v\0.0.1.pkl"
 
 
 # MISC

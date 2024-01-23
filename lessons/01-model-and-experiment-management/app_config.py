853a3b3eadad3f16c497b9d1f3883c1dd1239a41
unknown <stephanie.-@edu.devinci.fr>
2024-01-23 16:21:56 +0100

    Committing all changes

---
+++ lessons/01-model-and-experiment-management/app_config.py
@@ -0,0 +1,13 @@
+# MODELS
+MODEL_VERSION = "0.0.1"
+PATH_TO_PIPELINE = ...  # TODO: fill in the path to the pipeline
+
+
+# MISC
+APP_TITLE = "TripDurationPredictionApp"
+APP_DESCRIPTION = (
+    "A simple API to predict trip duration in minutes "
+    "for NYC yellow taxi trips, given a pickup, a dropoff location "
+    "and a passenger count."
+)
+APP_VERSION = "0.0.1"

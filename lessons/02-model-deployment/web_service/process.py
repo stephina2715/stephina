cb748c729d9e5196e266533734446e61d1b96a67
unknown <stephanie.-@edu.devinci.fr>
2024-01-23 19:33:42 +0100

    Your commit message here

--- lessons/02-model-deployment/web_service/process.py
+++ lessons/02-model-deployment/web_service/process.py
@@ -1,35 +1,12 @@
-from fastapi import Request
-import numpy as np
-from pydantic import BaseModel
-from typing import List
-import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 import pickle
+from app_config import PATH_TO_PREPROCESSOR
 
-# class PredictDurationInput(BaseModel):
-#     PULocationID: int = 264
-#     DOLocationID: int = 264
-#     passenger_count: int = 1
+# preprocess_path = "C:/Git/Stephanie/stephina/lessons/02-model-deployment/web_service/local_models/preprocessing.pkl"
 
-# def input_values(input_data) -> np.ndarray:
-#     if isinstance(input_data, PredictDurationInput):
-#         data = np.array([input_data.PULocationID, input_data.DOLocationID, input_data.passenger_count])
-#     else:
-#         data = np.array([input_data["PULocationID"], input_data["DOLocationID"], input_data["passenger_count"]])
+def save_picked(path: str, dv: DictVectorizer):
+    with open(path, "wb") as f:
+        pickle.dump(dv, f)
 
-#     # Convert all elements to strings
-#     data = data.astype(str)
-
-#     return data
-
-def encode_categorical_cols(df: pd.DataFrame, categorical_cols: List[str] = None) -> pd.DataFrame:
-    if categorical_cols is None:
-        categorical_cols = ["PULocationID", "DOLocationID", "passenger_count"]
-    df[categorical_cols] = df[categorical_cols].fillna(-1).astype("int")
-    df[categorical_cols] = df[categorical_cols].astype("str")
-    return df
-
-def load_preprocessor(path: str):
-    with open(path, "rb") as f:
-        loaded_obj = pickle.load(f)
-    return loaded_obj
+dv = DictVectorizer()
+save_picked(PATH_TO_PREPROCESSOR, dv)

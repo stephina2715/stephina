cea7367a2423472b412d459abe12b53a8efff3b6
unknown <stephanie.-@edu.devinci.fr>
2024-01-23 21:36:38 +0100

    Your commit message here

--- lessons/01-model-and-experiment-management/app_config.py
+++ lessons/01-model-and-experiment-management/app_config.py
@@ -1,6 +1,6 @@
 # MODELS
 MODEL_VERSION = "0.0.1"
-PATH_TO_PREPROCESSOR = f"/C:/Git/Stephanie/stephina/lessons/02-model-deployment/web_service/local_models/dv_v/{MODEL_VERSION}.pkl"
+PATH_TO_PREPROCESSOR = f"C:/Git/Stephanie/stephina/lessons/01-model-and-experiment-management/{MODEL_VERSION}.pkl"
 PATH_TO_MODEL = f"/C:/Git/Stephanie/stephina/lessons/02-model-deployment/web_service/local_models/model_v/{MODEL_VERSION}.pkl"
 CATEGORICAL_COLS = ["PUlocationID", "DOlocationID", "passenger_count"]
 
